f8c60485e5012dc4140a02a5a532fd525add3517
Grigoriy <grisha101202@yandex,ru>
2020-11-02 19:41:13 +0300

    ended dop 1

---
+++ src/DOPONE.java
@@ -0,0 +1,9 @@
+import java.util.Scanner;
+
+public class DOPONE {
+    public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+        int a = in.nextInt();
+        System.out.println(a % 10);
+    }
+}
